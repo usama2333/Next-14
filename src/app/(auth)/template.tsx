490615f2816578dedeb0b7134cc1dce02a32d2e0
usama2333 <usama.wizz@gmail.com>
2024-09-24 18:56:45 +0500

    Template

--- src/app/(auth)/template.tsx
+++ src/app/(auth)/template.tsx
@@ -2,7 +2,9 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 import './styles.css';
+
 
   
 const navLinks = [
@@ -17,10 +19,23 @@ const navLinks = [
     children: React.ReactNode
   }) {
     const pathname = usePathname();
+    const [input, setInput] = useState("");
     return (
         <div>
+            <div>
+                 {/* on layout page the state is preserved for child routes like 
+                this input value is same for login , register, forgot-password*/}
+
+                {/*In template page the state is not preserved for child routes like 
+               this input value is not same for login , register, forgot-password
+               It recreates new instance of componnet is mounted and 
+               Dom is recreated state is not preserved and effects are re-syncronized*/}
+               
+                <input value={input} onChange={(e) => setInput(e.target.value)} />
+            </div>
             {navLinks.map((link) => {
                 const isActive = pathname.startsWith(link.href)
+    
                 return (
                     <Link href={link.href} key={link.name} 
                      className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}
